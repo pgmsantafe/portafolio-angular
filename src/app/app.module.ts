826bd1b2a86213a7b78a0b94104655bcf6d8d188
Pablo Manozzo <pgmsantafe@hotmail.com>
2020-11-19 09:01:51 -0300

    listo para produccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
+// Rutas
+import { AppRoutingModule } from './app-routing.module';
+
+
+
 import { AppComponent } from './app.component';
 import { HgeaderComponent } from './shared/hgeader/hgeader.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { SearchComponent } from './pages/search/search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HgeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
     ],
   providers: [],
   bootstrap: [AppComponent]
